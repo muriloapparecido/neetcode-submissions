798f36d9267dc6dea013d13f8dc4c17362a0e046
Murilo Pinto Alves Apparecido <murilo.apparecido@gmail.com>
2026-05-31 13:49:44 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+
+        for w in strs:
+            key = tuple(sorted(w))
+            result[key].append(w)
+        
+        return list(result.values())
