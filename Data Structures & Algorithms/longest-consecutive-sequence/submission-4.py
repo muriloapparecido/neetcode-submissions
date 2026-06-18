bf7b317ba6e061eb6f3f8eb736ca73638f78aa7f
Murilo Pinto Alves Apparecido <murilo.apparecido@gmail.com>
2026-06-17 23:07:31 -0400

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums.sort()
+        streak = set()
+        best = 0
+        if nums == []: 
+            return 0
+        for i in range(1, len(nums)):
+            if nums[i] in streak:
+                continue
+            if nums[i] == nums[i-1] + 1:
+                streak.add(nums[i])
+            else: 
+                if len(streak) + 1 > best: 
+                    best = len(streak) + 1
+                streak.clear()
+
+
+        return max(best,len(streak) + 1)
